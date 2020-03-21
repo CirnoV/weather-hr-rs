cc0a42b3c783aecaa5c020ec64117e5e469b44df
CirnoV <cirnov59@gmail.com>
2020-03-21 23:41:22 +0900

    refactor: remove easy-scraper, clipboard-win deps

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,7 @@ mod request;
 mod utils;
 mod weather;
 
-use clipboard_win::Clipboard;
-
 #[tokio::main]
 async fn main() {
-    let weather = weather::get_weather().await;
-    let json = serde_json::to_string_pretty(&weather).unwrap();
-    Clipboard::new().unwrap().set_string(&json).unwrap();
+    weather::get_weather().await;
 }
--- src/weather/forest_fire.rs
+++ src/weather/forest_fire.rs
@@ -1,8 +1,10 @@
-use easy_scraper::Pattern;
 use serde::Serialize;
-use std::collections::BTreeMap;
 
-use crate::request::request;
+use crate::{
+    request::request,
+    utils::{elem_to_string, parse_elem},
+};
+use scraper::{ElementRef, Html, Selector};
 
 const FOREST_FIRE_URI: &str = "http://forestfire.nifos.go.kr/mobile/jsp/fireGrade.jsp?cd=42&cdName=%EA%B0%95%EC%9B%90%EB%8F%84&subCd=42810&subCdName=%EC%9D%B8%EC%A0%9C%EA%B5%B0";
 
@@ -12,30 +14,22 @@ pub struct ForestFire {
 }
 
 pub async fn get_forest_fire() -> ForestFire {
-    let value = {
-        let document: String = request(FOREST_FIRE_URI).await.unwrap();
-        let pat: Pattern = Pattern::new(
-            r#"
-        <div class="greenTable">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>{{location}}</td>
-                        <td>{{rank}}</td>
-                        <td>{{value}}</td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-        "#,
-        )
-        .unwrap();
-
-        let data: Vec<BTreeMap<String, String>> = pat.matches(&document);
-        let inje = data.iter().find(|&x| x["location"] == "인제군").unwrap();
-        let value: Option<f64> = inje["value"].parse::<f64>().ok();
-        value
+    let html: String = match request(FOREST_FIRE_URI).await {
+        Ok(res) => res,
+        Err(_) => return ForestFire { value: None },
     };
+    let document = Html::parse_document(&html);
+    let tr = Selector::parse("div.greenTable > table > tbody > tr").unwrap();
+    let td = Selector::parse("td").unwrap();
+    let tr: Vec<ElementRef> = document.select(&tr).collect();
+    let value = tr.iter().find_map(|elem| {
+        let td = elem.select(&td).collect::<Vec<_>>();
+        let value: Option<f64> = match elem_to_string(&td[0]) == "인제군" {
+            true => parse_elem(&td[2]),
+            _ => None,
+        };
+        value
+    });
 
     ForestFire { value }
 }
