15e602a2532263aeb22cf73ce45a07e4759d31dc
CirnoV <cirnov59@gmail.com>
2020-03-31 22:18:40 +0900

    feat: implement pagination

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ extern crate regex;
 use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer};
 use chrono::prelude::*;
 use chrono::Duration;
+use serde::Serialize;
 use std::{io, sync::Mutex};
 
 mod request;
@@ -13,29 +14,95 @@ mod weather;
 
 type WeatherData = Vec<weather::Weather>;
 
-async fn index(
-    weather: web::Data<Mutex<WeatherData>>,
-    last_updated: web::Data<Mutex<DateTime<Local>>>,
-    _req: HttpRequest,
-) -> HttpResponse {
+#[derive(Debug, Serialize)]
+struct WeatherJson {
+    current: usize,
+    length: usize,
+    data: Option<weather::Weather>,
+}
+
+async fn update_weathers(now: DateTime<Local>, weathers: &web::Data<Mutex<WeatherData>>) {
+    let weather_data = weather::get_weather(now.timestamp_millis()).await;
+    {
+        let mut weathers = weathers.lock().unwrap();
+        match weathers.last() {
+            Some(last_weather) => {
+                let last_hr = Local.timestamp_millis(last_weather.timestamp).hour();
+                let current_hr = now.hour();
+                if last_hr == current_hr {
+                    weathers.pop();
+                }
+            }
+            None => (),
+        };
+        weathers.push(weather_data);
+        let inner: Vec<weather::Weather> = weathers
+            .clone()
+            .into_iter()
+            .filter(|w| w.timestamp > (now - Duration::days(3)).timestamp_millis())
+            .collect();
+        *weathers = inner;
+    }
+}
+
+async fn get_weathers(
+    pos: usize,
+    weathers: &web::Data<Mutex<WeatherData>>,
+    last_updated: &web::Data<Mutex<DateTime<Local>>>,
+) -> WeatherJson {
     let update_time = *last_updated.lock().unwrap() + Duration::minutes(3);
     let now = Local::now();
     if update_time <= now {
         *last_updated.lock().unwrap() = now;
-        let weather_data = weather::get_weather(now.timestamp_millis()).await;
-        {
-            let mut weather = weather.lock().unwrap();
-            weather.push(weather_data);
-            let inner: Vec<weather::Weather> = weather
-                .clone()
-                .into_iter()
-                .filter(|w| w.timestamp > (now - Duration::days(3)).timestamp_millis())
-                .collect();
-            *weather = inner;
-        };
+        update_weathers(now, &weathers).await;
         println!("Updated, {}", now);
     }
-    HttpResponse::Ok().json(&*weather.lock().unwrap())
+    let weather = weathers.lock().unwrap();
+    let length = weather.len();
+    let weather = match weather.get(pos) {
+        Some(weather) => Some(weather.clone()),
+        None => None,
+    };
+    let result = WeatherJson {
+        data: weather,
+        length,
+        current: pos,
+    };
+    result
+}
+
+async fn index(
+    weathers: web::Data<Mutex<WeatherData>>,
+    last_updated: web::Data<Mutex<DateTime<Local>>>,
+    _req: HttpRequest,
+) -> HttpResponse {
+    let pos = {
+        let length = (*weathers.lock().unwrap()).len();
+        match length > 0 {
+            true => length - 1,
+            false => 0,
+        }
+    };
+    let result = get_weathers(pos, &weathers, &last_updated).await;
+    HttpResponse::Ok().json(result)
+}
+
+async fn index_with_param(
+    weathers: web::Data<Mutex<WeatherData>>,
+    last_updated: web::Data<Mutex<DateTime<Local>>>,
+    _req: HttpRequest,
+    path: web::Path<(String,)>,
+) -> HttpResponse {
+    let length = { (*weathers.lock().unwrap()).len() };
+    let pos: usize = match path.0.parse() {
+        Ok(pos) => pos,
+        Err(_) => match length > 0 {
+            true => length - 1,
+            false => 0,
+        },
+    };
+    let result = get_weathers(pos, &weathers, &last_updated).await;
+    HttpResponse::Ok().json(result)
 }
 
 #[actix_rt::main]
@@ -55,6 +122,7 @@ async fn main() -> io::Result<()> {
             .wrap(middleware::Logger::default())
             // register simple handler
             .service(web::resource("/").to(index))
+            .service(web::resource("/{pos}").route(web::get().to(index_with_param)))
     })
     .bind("0.0.0.0:8080")?
     .run()
